766a21d6924c6e9180a4ad0b8da0028de045e64b
Asli Yilmaz <75321471+Asli-Yilmaz@users.noreply.github.com>
2022-05-10 21:56:08 +0300

    Add files via upload

---
+++ js/page5.js
@@ -0,0 +1,41 @@
+
+function temizle(){
+  document.getElementById("form").reset();
+  document.getElementsById("ad")="";
+  document.getElementsById("soyad")="";
+  document.getElementsById("email")="";
+  document.getElementsById("tarih")="";
+  document.getElementsById("cinsiyet")="";
+  document.getElementsById("sehir")="";
+  document.getElementsById("mesajkonu")="";
+  document.getElementsById("mesaj")="";
+  alert("hata");
+}
+function kontrol(){
+  var ad=document.getElementsById("ad");
+  var soyad=document.getElementsById("soyad");
+  var email=document.getElementsById("email");
+  var mesaj=document.getElementsById("mesaj");
+  var uyari;
+  if (ad==null){
+    uyari="İsim bilgisi boş geçilemez.Lütfen isim bilginizi giriniz.";
+    document.getElementsById("aduyari").innerHTML=uyari;
+    document.getElementsById("aduyari").classList.add("alert alert-warning");
+  }
+  else if (soyad=null) {
+    uyari="Soyad bilgisi boş geçilemez.Lütfen soyad bilginizi giriniz.";
+    document.getElementsById("soyaduyari").innerHTML=uyari;
+    document.getElementsById("soyaduyari").classList.add("alert alert-warning");
+  } 
+  else if(email==null){
+  uyari="Email bilgisi boş geçilemez.Sizinle iletişime geçebilmem için lütfen e-mail adresinizi giriniz.";
+  document.getElementsById("emailuyari").innerHTML=uyari;
+  document.getElementsById("emailuyari").classList.add("alert alert-warning");
+  }
+  else if( mesajkonu==""){
+    uyari="Mesaj kısmı boş geçişlemez.Lütfen iletmek istedğiniz mesajınızı yazınız.";
+    document.getElementsById("mesajuyari").classList.add("alert alert-warning");
+    document.getElementsById("mesajuyari").innerHTML=uyari;
+  }
+}
+
